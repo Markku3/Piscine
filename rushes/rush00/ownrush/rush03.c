a5d1f0ca4b68c8f2ddfb6e4b69caec1b44df1698
Markku3 <139809741+Markku3@users.noreply.github.com>
2023-07-19 13:03:37 +0300

    Add files via upload

---
+++ rushes/rush00/ownrush/rush03.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush03.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: walnaimi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/01 17:55:21 by walnaimi          #+#    #+#             */
+/*   Updated: 2023/07/01 19:00:01 by walnaimi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_print(int len, char begin_char, char midel_char, char end_char)
+{
+	int	letter_counter;
+
+	letter_counter = 1;
+	while (letter_counter <= len)
+	{
+		if (letter_counter == 1)
+			ft_putchar(begin_char);
+		else if (letter_counter == len)
+			ft_putchar(end_char);
+		else
+			ft_putchar(midel_char);
+		letter_counter++;
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	line_counter;
+
+	line_counter = 1;
+	if (x >= 1 && y >= 1)
+	{
+		while (line_counter <= y)
+		{
+			if (line_counter == 1)
+				ft_print(x, 'A', 'B', 'C');
+			else if (line_counter == y)
+				ft_print(x, 'A', 'B', 'C');
+			else
+				ft_print(x, 'B', ' ', 'B');
+			line_counter++;
+		}
+	}
+}
