f3bc3b6f80517940133afd9ddca0a396eac7f18b
Markku3 <139809741+Markku3@users.noreply.github.com>
2023-07-19 12:42:28 +0300

    Add files via upload

---
+++ C01/ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhynynen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/03 21:09:39 by mhynynen          #+#    #+#             */
+/*   Updated: 2023/07/03 23:06:56 by mhynynen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_ultimate_div_mod(int *a, int *b)
+{
+	int	div;
+	int	mod;
+
+	div = *a / *b;
+	mod = *a % *b;
+	*a = div;
+	*b = mod;
+}
+/*
+int	main(void)
+{
+	int	x;
+	int	y;
+
+	x = 16;
+	y = 5;
+	ft_ultimate_div_mod(&x, &y);
+	printf("div %d | mod %d", x, y);
+}*/
