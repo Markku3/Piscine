40ac59e4acc026cb3d6be1591734e575552dc2b2
Markku3 <139809741+Markku3@users.noreply.github.com>
2023-07-19 12:43:25 +0300

    Add files via upload

---
+++ c04/ex02/ft_putnbr.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhynynen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/09 14:38:21 by mhynynen          #+#    #+#             */
+/*   Updated: 2023/07/10 17:13:00 by mhynynen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*#include <stdio.h>
+#include <string.h>*/
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb *= -1;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+	{
+		ft_putchar(nb + 48);
+	}
+}
+
+/*int	main(void)
+{
+	ft_putnbr(-2147483648);
+	write(1, "\n", 1);
+	return (0);
+}*/
